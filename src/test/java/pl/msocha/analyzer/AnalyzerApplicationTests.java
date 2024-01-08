c55844dbcec9312b7111dc0e4997deec32aa54d1
Michał Socha <michal..socha.88@gmail.com>
2024-01-08 22:31:21 +0100

    Initial commit

---
+++ src/test/java/pl/msocha/analyzer/AnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.msocha.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
